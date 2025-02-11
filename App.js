e1a94dd5ddf76d4c76f70fc30044a02a9f054020
kranjeet-da <k.ranjeetkumar798@gmail.com>
2025-02-11 22:00:00 +0530

    Added files

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
-import React from 'react';
-import { ReactDOM, createRoot } from 'react-dom/client';
+import React, { useState, useEffect } from "react";
 
-//React.createElement is a JS Object.
-const heading = React.createElement(
-    "div", //Which tag to be created?
-    {id : "parent"}, //attributes
-    React.createElement("div", {id : "child"}, 
-    [React.createElement("h1", {id : "child1"}, "I'm a h1 tag"),
-    React.createElement("h2", {id : "child2"}, "I'm a h2 tag")])
-);//what to display.?
+const ProductsList = () => {
+  const [products, setProducts] = useState([]);
+  // const [loading, setLoading] = useState(true);
+  // const [error, setError] = useState(null);
 
-//Rendering react
-const root = createRoot(document.getElementById("root"));
+  // useEffect(() => {
+  //   fetch("https://fakestoreapi.com/products")
+  //     .then((res) => {
+  //       if (!res.ok) throw new Error("Failed to fetch products");
+  //       return res.json();
+  //     })
+  //     .then((data) => {
+  //       setProducts(data);
+  //       // setLoading(false);
+  //     })
+  //     // .catch((error) => {
+  //     //   setError(error.message);
+  //     //   setLoading(false);
+  //     // });
+  // }, []);
 
-/*
-render() => renders a JS object(heading) and 
-creates the tag which browser understands puts it up in the DOM.
-*/
-root.render(heading);
+  const produ = fetch("https://fakestoreapi.com/products")
+  console.log(produ)
+
+  // if (loading) return <div className="text-center text-xl font-semibold">Loading...</div>;
+
+  // if (error) return <div className="text-center text-red-500">{error}</div>;
+
+  return (
+    <div >
+      {produ.map((product) => (
+        <div key={product.id}>
+          <img src={product.image} alt={product.title} className="object-contain" />
+          <h3 className="text-lg font-semibold mt-2">{product.title}</h3>
+          <p className="text-gray-700 mt-1">${product.price}</p>
+          <button className="mt-2 bg-blue-500 hover:bg-blue-600 text-white px-3 py-1 rounded transition">
+            View Details
+          </button>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default ProductsList;
